56d172c86022995a907230790a11a4cdbe047dfd
Victoriia Koval <149317864+Vika-Koval@users.noreply.github.com>
2024-11-04 00:13:03 +0200

    Update Flower.java

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -1,21 +1,26 @@
 package flower.store;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Setter @Getter @NoArgsConstructor
+public class Flower extends FlowerInfo {
 
-
-@Setter
-public class Flower {
-    @Getter
-    private double sepalLength;
-    private FlowerColor color;
-    @Getter
     private double price;
-    @Getter
-    private FlowerType flowerType;
 
-    public String getColor() {
-        return color.toString();
+    public Flower(Flower flower) {
+        this.price = flower.price;
+        this.setSepalLength(flower.getSepalLength());
+        this.setColor(FlowerColor.getColor(flower.getColor()));
+        this.setType(flower.getType());
+    }
+
+    public Flower(double price, double sepalLength,
+                 FlowerColor color, FlowerType type) {
+        this.setPrice(price);
+        this.setSepalLength(sepalLength);
+        this.setColor(color);
+        this.setType(type);
     }
 }
