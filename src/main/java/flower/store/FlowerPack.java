c980bfb8e5912d0b01b7d14d937fbb66ad18577a
Victoriia Koval <149317864+Vika-Koval@users.noreply.github.com>
2024-11-04 00:14:13 +0200

    Update FlowerPack.java

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -1,6 +1,23 @@
 package flower.store;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class FlowerPack {
     private Flower flower;
-    private int quantity;
+    private int count;
+
+    public FlowerPack(Flower flower, int count) {
+        this.flower = new Flower(flower);
+        this.count = count;
+    }
+
+    public double getPrice() {
+        return flower.getPrice() * count;
+    }
+    
+    public boolean matches(FlowerInfo flowerInfo) {
+        return this.flower.matches(flowerInfo);
+    }
 }
