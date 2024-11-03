6361de7ca63ed422fc997acf5a1dfda3bdf893aa
Victoriia Koval <149317864+Vika-Koval@users.noreply.github.com>
2024-11-04 00:13:48 +0200

    Update FlowerColor.java

--- src/main/java/flower/store/FlowerColor.java
+++ src/main/java/flower/store/FlowerColor.java
@@ -1,18 +1,27 @@
 package flower.store;
 
-public enum FlowerColor {
-    /**
-     * Flower colors.
-     */
-    RED("#FF0000"), BLUE("#0000FF");
-    private final String stringRepresentation;
+import lombok.Getter;
 
-    FlowerColor(String stringRepresentation) {
-        this.stringRepresentation = stringRepresentation;
+@Getter
+public enum FlowerColor {
+    RED("#FF0000"), BLUE("#0000FF");
+    private final String colorCode;
+
+    FlowerColor(String colorCode) {
+        this.colorCode = colorCode;
     }
 
     @Override
     public String toString() {
-        return stringRepresentation;
+        return colorCode;
     }
+
+    public static FlowerColor getColor(String color) {
+        for (FlowerColor flowerColor : FlowerColor.values()) {
+            if (flowerColor.toString().equals(color)) {
+                return flowerColor;
+            }
+        }
+        return null;
+    }  
 }
